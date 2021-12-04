c6a032487bfab62976e9e2f4fefc85f41dc4d979
hbcoskun <94205241+hbcoskun@users.noreply.github.com>
2021-12-04 18:34:18 -0500

    Update COVID_PortfolioProjects.sql

--- COVID_PortfolioProjects.sql
+++ COVID_PortfolioProjects.sql
@@ -1,3 +1,8 @@
+/**
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+**/
+
 use Portfolio;
 drop table if exists Portfolio.CovidDeaths;
 
